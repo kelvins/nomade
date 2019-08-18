b1609fbc6acb62b8b6e34ad4fc3f883fdf55bde1
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2019-08-17 22:19:07 -0300

    Create nomade init command

--- nomade/main.py
+++ nomade/main.py
@@ -1,34 +1,47 @@
 import click
 
+from nomade import Nomade
+
 
 @click.group()
 def cli():
     pass
 
 
+@cli.command(help='Init the migration manager')
+def init():
+    Nomade.init()
+
+
 @cli.command(help='Create a new migration')
+@cli.argument('name', help='Migration name')
 def migrate():
-    pass
+    nomade = Nomade()
+    nomade.migrate()
 
 
 @cli.command(help='Upgrade migrations')
 def upgrade():
-    pass
+    nomade = Nomade()
+    nomade.upgrade()
 
 
 @cli.command(help='Downgrade migrations')
 def downgrade():
-    pass
+    nomade = Nomade()
+    nomade.downgrade()
 
 
 @cli.command(help='Show migrations history')
 def history():
-    pass
+    nomade = Nomade()
+    nomade.history()
 
 
 @cli.command(help='Show the current migration')
 def current():
-    pass
+    nomade = Nomade()
+    nomade.current()
 
 
 if __name__ == '__main__':

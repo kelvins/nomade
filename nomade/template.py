e424f4b5bcc04a4e1d1bdeec383d44d2ffd87b7c
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2019-08-17 22:11:11 -0300

    Create migration template

---
+++ nomade/template.py
@@ -0,0 +1,17 @@
+"""
+Name: {name}
+ID: {up_migration}
+Date: {date}
+"""
+
+# Nomade migration identifiers
+migration = '{up_migration}'
+down_migration = '{down_migration}'
+
+
+def upgrade():
+    """Write your upgrade statements here."""
+
+
+def downgrade():
+    """Write your downgrade statements here."""

3375d373a64e10240d698c072f3fa2c9954a8ea8
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2019-08-21 23:33:09 -0300

    Create log module

---
+++ nomade/log.py
@@ -0,0 +1,44 @@
+from colorama import Fore
+
+
+def colorize(fore_color):
+    def decorator(func):
+        def wrapper(content):
+            print(fore_color + content + Fore.RESET)
+        return wrapper
+    return decorator
+
+
+@colorize(Fore.RESET)
+def default(content):
+    pass
+
+
+@colorize(Fore.BLUE)
+def debug(content):
+    pass
+
+
+@colorize(Fore.CYAN)
+def info(content):
+    pass
+
+
+@colorize(Fore.GREEN)
+def success(content):
+    pass
+
+
+@colorize(Fore.YELLOW)
+def warning(content):
+    pass
+
+
+@colorize(Fore.MAGENTA)
+def error(content):
+    pass
+
+
+@colorize(Fore.RED)
+def critical(content):
+    pass

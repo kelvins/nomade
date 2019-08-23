1641c646408edff1ea7ce7ca9da357f712750995
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2019-08-23 00:11:57 -0300

    Store upgrade and downgrade functions

--- nomade/migration.py
+++ nomade/migration.py
@@ -11,6 +11,8 @@ class Migration:
         self.name = name
         self.date = date
         self.down_migration = down_migration
+        self.upgrade = None
+        self.downgrade = None
 
     def __repr__(self):
         return f'<Migration id={self.id}, down={self.down_migration}>'
@@ -71,7 +73,9 @@ class Migration:
             id=module.curr_migration,
             name=module.migration_name,
             date=module.migration_date,
-            down_migration=module.down_migration or None
+            down_migration=module.down_migration or None,
+            upgrade=module.upgrade,
+            downgrade=module.downgrade,
         )
 
     @staticmethod

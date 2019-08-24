142a35e6435eb0764136236db1196cf705a64455
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2019-08-24 12:43:16 -0300

    Improve CLI documentation

--- nomade/main.py
+++ nomade/main.py
@@ -13,52 +13,77 @@ def cli():
 
 @cli.command()
 def version():
-    """Show Nomade version."""
+    """Show Nomade version.
+
+    Example:\n
+        nomade version
+    """
     click.secho(f'Nomade ({__version__})', fg=level.INFO)
 
 
 @cli.command()
 def init():
-    """Init a Nomade project."""
+    """Init a Nomade project.
+
+    Example:\n
+        nomade init
+    """
     nomade.Nomade.init()
 
 
 @cli.command()
-@click.argument('name')
+@click.argument('name', required=True)
 def migrate(name):
-    """Create a new migration."""
-    nomade = nomade.Nomade()
-    nomade.migrate(name)
+    """Create a new migration.
+
+    Example:\n
+        nomade migrate "Create X table"
+    """
+    nomade.Nomade().migrate(name)
 
 
 @cli.command()
-@click.argument('steps', required=False)
+@click.argument('steps', required=True)
 def upgrade(steps):
-    """Upgrade migrations."""
-    nomade = nomade.Nomade()
-    nomade.upgrade(steps)
+    """Upgrade migrations.
+
+    Example:\n
+        nomade upgrade 1\n
+        nomade upgrade head
+    """
+    nomade.Nomade().upgrade(steps)
 
 
 @cli.command()
-@click.argument('steps', required=False)
+@click.argument('steps', required=True)
 def downgrade(steps):
-    """Downgrade migrations."""
-    nomade = nomade.Nomade()
-    nomade.downgrade(steps)
+    """Downgrade migrations.
+
+    Example:\n
+        nomade downgrade 1\n
+        nomade downgrade tail
+    """
+    nomade.Nomade().downgrade(steps)
 
 
 @cli.command()
 def history():
-    """Show migrations history."""
-    nomade = nomade.Nomade()
-    nomade.history()
+    """Show migrations history.
+
+    Example:\n
+        nomade history
+    """
+    nomade.Nomade().history()
 
 
 @cli.command()
 def current():
-    """Show the current migration."""
-    nomade = nomade.Nomade()
-    nomade.current()
+    """Show the current migration.
+
+    Example:\n
+        nomade current
+    """
+    nomade.Nomade().current()
 
 
 if __name__ == '__main__':
